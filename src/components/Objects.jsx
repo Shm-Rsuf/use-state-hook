f60973869771f1e97fef45dcf26e83974f55e5ae
shm_usuf <shmusuf@gmail.com>
2022-11-22 20:01:34 +0600

    use hook added

--- src/components/Objects.jsx
+++ src/components/Objects.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
+const messi = {
+  firstName: "Leo",
+  lastName: "Messi",
+  jersyNum: 10,
+};
+
 const Objects = () => {
-  return <div>Objects</div>;
+  const [player, setPlayer] = useState({});
+
+  const addPropertyHandler = () => {
+    setPlayer({ ...player, ...messi });
+    console.log(player);
+  };
+
+  const makeGoalHandler = () => {
+    const goal = Math.floor(Math.random() * 10 + 1);
+    setPlayer({ ...player, goal: goal });
+  };
+
+  return (
+    <div>
+      <h2>
+        Player Name: {player.firstName} {player.lastName}
+      </h2>
+      <h2>Jersy Number: {player.jersyNum} </h2>
+      <h2>Goals: {player.goal}</h2>
+      <button onClick={addPropertyHandler}>Add Property</button>
+      <button onClick={makeGoalHandler}>make goals</button>
+    </div>
+  );
 };
 
 export default Objects;
